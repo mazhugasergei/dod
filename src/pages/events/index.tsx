dec103d6a6962739ebbcbdf864fb4bad3c5b6ecf
Markus Wedler <markuswedler8@gmail.com>
2023-03-20 17:36:59 +1000

    .

--- src/pages/events/index.tsx
+++ src/pages/events/index.tsx
@@ -15,10 +15,9 @@ export default () => {
       </Head>
       <Header />
       <main className="wrapper events">
-        <h1>Программа Дня открытых дверей</h1>
+        {/* <h1>Программа Дня открытых дверей</h1>
         <h2>Корпус А</h2>
         <div className="item">
-          {/* <img src="https://raw.githubusercontent.com/markuswedler/dod/main/public/assets/images/floors/%D0%9A%D0%B0%D1%80%D1%82%D0%B0%20%D0%906.svg" alt="" /> */}
           <div className="img" style={{ background: "url('https://raw.githubusercontent.com/markuswedler/dod/main/public/assets/images/floors/%D0%9A%D0%B0%D1%80%D1%82%D0%B0%20%D0%906.svg')" }} />
           <div className="right lightblue">
             <h3>Холл 6 уровень</h3>
@@ -26,9 +25,7 @@ export default () => {
             <p>Основной ресепшен / запись на экскурсии</p>
           </div>
         </div>
-        {/* <hr /> */}
         <div className="item">
-          {/* <img src="https://raw.githubusercontent.com/markuswedler/dod/main/public/assets/images/floors/%D0%9A%D0%B0%D1%80%D1%82%D0%B0%20%D0%905.svg" alt="" /> */}
           <div className="img" style={{ background: "url('https://raw.githubusercontent.com/markuswedler/dod/main/public/assets/images/floors/%D0%9A%D0%B0%D1%80%D1%82%D0%B0%20%D0%905.svg')" }} />
           <div className="right orange">
             <h3>Холл 5 уровень</h3>
@@ -36,9 +33,7 @@ export default () => {
             <p>Приемная комиссия / Договорной отдел / Представители банков: Сбер и Газпром / Служба проректора по международным отношениям: «Возможности ДВФУ: стажировки и обучение за рубежом» / Дополнительное профессиональное образование</p>
           </div>
         </div>
-        {/* <hr /> */}
         <div className="item">
-          {/* <img src="https://raw.githubusercontent.com/markuswedler/dod/main/public/assets/images/floors/%D0%9A%D0%B0%D1%80%D1%82%D0%B0%20%D0%904.svg" alt="" /> */}
           <div className="img" style={{ background: "url('https://raw.githubusercontent.com/markuswedler/dod/main/public/assets/images/floors/%D0%9A%D0%B0%D1%80%D1%82%D0%B0%20%D0%904.svg')" }} />
           <div className="right">
             <h3>Холл 4 уровень</h3>
@@ -46,9 +41,7 @@ export default () => {
             <p>Ярмарка Институтов (12 стендов) + Военный учебный центр</p>
           </div>
         </div>
-        {/* <hr /> */}
         <div className="item">
-          {/* <img src="https://raw.githubusercontent.com/markuswedler/dod/main/public/assets/images/floors/%D0%9A%D0%B0%D1%80%D1%82%D0%B0%20%D0%904.svg" alt="" /> */}
           <div className="img" style={{ background: "url('https://raw.githubusercontent.com/markuswedler/dod/main/public/assets/images/floors/%D0%9A%D0%B0%D1%80%D1%82%D0%B0%20%D0%904.svg')" }} />
           <div className="right">
             <h3>Кафе «Аякс» 4 уровень</h3>
@@ -61,7 +54,6 @@ export default () => {
 
         <h2>Корпус В</h2>
         <div className="item">
-          {/* <img src="https://raw.githubusercontent.com/markuswedler/dod/main/public/assets/images/floors/%D0%9A%D0%B0%D1%80%D1%82%D0%B0%20%D0%926.svg" alt="" /> */}
           <div className="img" style={{ background: "url('https://raw.githubusercontent.com/markuswedler/dod/main/public/assets/images/floors/%D0%9A%D0%B0%D1%80%D1%82%D0%B0%20%D0%926.svg')" }} />
           <div className="right blue">
             <h3>Синий зал 6 уровень</h3>
@@ -73,9 +65,7 @@ export default () => {
             <p>Открытая встреча с директорами Институтов о возможностях ДВФУ, партнеров, ответы на вопросы участников. ШЭМ.</p>
           </div>
         </div>
-        {/* <hr /> */}
         <div className="item">
-          {/* <img src="https://raw.githubusercontent.com/markuswedler/dod/main/public/assets/images/floors/%D0%9A%D0%B0%D1%80%D1%82%D0%B0%20%D0%926.svg" alt="" /> */}
           <div className="img" style={{ background: "url('https://raw.githubusercontent.com/markuswedler/dod/main/public/assets/images/floors/%D0%9A%D0%B0%D1%80%D1%82%D0%B0%20%D0%926.svg')" }} />
           <div className="right lightblue">
             <h3>Средний зал 6 уровень (центральный лекторий)</h3>
@@ -87,16 +77,15 @@ export default () => {
             <p>Открытая встреча с директорами Институтов о возможностях ДВФУ, партнеров, ответы на вопросы участников. ПИ.</p>
           </div>
         </div>
-        {/* <hr /> */}
         <div className="item">
-          {/* <img src="https://raw.githubusercontent.com/markuswedler/dod/main/public/assets/images/floors/%D0%9A%D0%B0%D1%80%D1%82%D0%B0%20%D0%926.svg" alt="" /> */}
           <div className="img" style={{ background: "url('https://raw.githubusercontent.com/markuswedler/dod/main/public/assets/images/floors/%D0%9A%D0%B0%D1%80%D1%82%D0%B0%20%D0%926.svg')" }} />
           <div className="right orange">
             <h3>Холл 6 уровень</h3>
             <h4>9:00 – 17:00</h4>
             <p>Дополнительный ресепшен Ярмарка студенческих организаций, студенческих активностей / Студенческое научное общество / AR/VR Мобильная телестудия для выступлений проректоров, директоров Институтов, руководителей направлений.</p>
           </div>
-        </div>
+        </div> */}
+        <img src="" alt="" />
       </main>
       <Footer />
     </>
