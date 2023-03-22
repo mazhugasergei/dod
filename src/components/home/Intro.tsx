5ed280ad20a08c69b2f61ef8bca07953ff6a6865
Markus Wedler <markuswedler8@gmail.com>
2023-03-22 16:08:03 +1000

    .

--- src/components/home/Intro.tsx
+++ src/components/home/Intro.tsx
@@ -2,6 +2,7 @@ const Intro = () => {
   return (
     <section className="intro wrapper grid">
       <p>Приглашаем абитуриентов и родителей! Вы сможете узнать о подготовке к поступлению в Дальневосточный федеральный университет.</p>
+      <img src="https://raw.githubusercontent.com/markuswedler/dod/main/public/assets/images/campus.jpeg" alt="" />
     </section>
   )
 }
