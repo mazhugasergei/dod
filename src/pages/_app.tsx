2d2e7063cb5436658cbc2875951ad2e71c3d68bd
Markus Wedler <markuswedler8@gmail.com>
2023-03-22 15:34:29 +1000

    .

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,12 +11,12 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Script>
         {`
-          (function(m:any,e:any,t:any,r:any,i:any,k:any,a:any){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-          m[i].l=new Date();
+          (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+          m[i].l=1*new Date();
           for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
           k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-          (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym", null, null);
-
+          (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+          
           ym(92890214, "init", {
               clickmap:true,
               trackLinks:true,
