dc78f8f1ac6697a0c83d6d0491f4357587092fc8
DimaVerd <128450750+DimaVerd@users.noreply.github.com>
2023-03-23 15:12:05 +1000

    Update Hero.tsx

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -4,7 +4,7 @@ const Hero = () => {
       <div className="wrapper">
         <div className="cont">
           <p>1 апреля 2023</p>
-          <h1>День<br/>открытых<br/>дверей!</h1>
+          <h1>День<br/>открытых<br/>дверей</h1>
         </div>
         <img src="https://github.com/markuswedler/dod/blob/main/public/assets/images/vlad.png?raw=true" alt="" />
       </div>
