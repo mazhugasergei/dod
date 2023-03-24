ece02f639f0bfd51aa3a167bea5fd928a46659b6
Markus Wedler <markuswedler8@gmail.com>
2023-03-24 11:53:52 +1000

    .

--- src/pages/excursions/index.tsx
+++ src/pages/excursions/index.tsx
@@ -1,8 +1,18 @@
+import Head from "next/head"
+
 const Excursions = () => {
   return (
-    <main className="wrapper excursions">
-      <p>Раздел находится в разработке</p>
-    </main>
+    <>
+      <Head>
+        <title>День открытых дверей ДВФУ</title>
+        <meta name="description" content="1 апреля Дальневосточный федеральный университет в очном формате проводит День открытых дверей. Приглашаем абитуриентов и родителей!" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="wrapper excursions">
+        <p>Раздел находится в разработке</p>
+      </main>
+    </>
   )
 }
  
