4cb7955dbfc29f38430f04715f1ba16e5f53822f
DimaVerd <128450750+DimaVerd@users.noreply.github.com>
2023-03-24 10:41:43 +1000

    Update Intro.tsx

--- src/components/home/Intro.tsx
+++ src/components/home/Intro.tsx
@@ -2,7 +2,8 @@ const Intro = () => {
   return (
     <section className="intro wrapper grid">
       <div>
-        <p>Приглашаем абитуриентов и родителей! Вы сможете узнать о подготовке к поступлению в Дальневосточный федеральный университет.</p>
+        <p>Приглашаем абитуриентов и родителей!</p>
+        <p>Вы сможете узнать о подготовке к поступлению в Дальневосточный федеральный университет.</p>
         <p>ДВФУ – Действуй, Выбирай, Формируй Успех!</p>
         </div>
       <img src="https://raw.githubusercontent.com/markuswedler/dod/main/public/assets/images/campus.jpeg" alt="" />
