dae7b1991845550b98c88bbe71c01e769e238e20
antipdi <61740732+antipdi@users.noreply.github.com>
2024-03-26 10:38:44 +1000

    Update Hero.tsx

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
     <section className="hero">
       <div className="wrapper">
         <div className="cont">
-          <p>30 марта 2024</p>
+          <p>30 марта 2024, 10:00</p>
           <h1>День<br/>открытых<br/>дверей</h1>
         </div>
         <img src={Fox.src} alt="" />
