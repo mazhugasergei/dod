bc70df7e17b2db4a4ce0e5d5ae352e37beee4b50
antipdi <61740732+antipdi@users.noreply.github.com>
2024-04-04 14:00:02 +1000

    Add files via upload

---
+++ src/components/home/Slavda.tsx
@@ -0,0 +1,13 @@
+// images
+import Image from "@/assets/images/Slavda.pdf"
+
+const Slavda = () => {
+  return (
+    <section className="intro wrapper grid">
+
+      <img src={Image.src} alt="" />
+    </section>
+  )
+}
+ 
+export default Slavda
